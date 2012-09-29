b1ac68e6863f306e3b20cdec8f0760992b04f744
Esteban Bouza <esteban.bouza@gmail.com>
2012-09-30 00:22:46 +0200

    Added test for failed connection

--- EBRequest/EBRequestTests/EBRequestTests.m
+++ EBRequest/EBRequestTests/EBRequestTests.m
@@ -52,6 +52,69 @@ static const NSTimeInterval defaultTimeout = 10;
     }
     
     dispatch_release(semaphore);
+    
+    [request release];
+}
+
+- (void)testRegularAutoreleasedAsyncConnection {
+    
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    
+    EBRequest *request = [EBRequest requestWithURL:[NSURL URLWithString:testURLString]];
+    
+    request.completionBlock = ^(id responseData) {
+        dispatch_semaphore_signal(semaphore);
+        
+        STAssertNotNil(responseData, @"Response data should be initialized");
+        
+    };
+    
+    request.errorBlock = ^(NSError *error) {
+        dispatch_semaphore_signal(semaphore);
+
+        STFail(@"Request failed");
+    };
+    
+    BOOL status = [request start];
+    
+    STAssertTrue(status, @"Request could not be started");
+    
+    while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW)) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:defaultTimeout]];
+    }
+    
+    dispatch_release(semaphore);
+    
+}
+
+- (void)testFailedConnection {
+    
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+
+    EBRequest *request = [EBRequest requestWithURL:[NSURL URLWithString:@"http://fals3-d0m4in.com"]];
+    
+    request.completionBlock = ^(id responseData) {
+        dispatch_semaphore_signal(semaphore);
+
+        STFail(@"Completion block should not be executed");
+    };
+    
+    request.errorBlock = ^(NSError *error) {
+        dispatch_semaphore_signal(semaphore);
+
+        STAssertNotNil(error, @"An error is supposed to happen");
+        STAssertEquals(error.code, -1003, @"The server should not be found");
+    };
+    
+    [request start];
+    
+    while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW)) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:defaultTimeout]];
+    }
+    
+    dispatch_release(semaphore);
+    
+    [request release];
 }
 
 @end
