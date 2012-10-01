15e57a3d5a4964ed6c1d38eb9095bae21f73a132
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-01 20:37:49 +0200

    Added EBJSONRequest for feed-class direct mapping

---
+++ EBRequest/EBRequest/EBJSONRequest.h
@@ -0,0 +1,13 @@
+//
+//  EBJSONRequest.h
+//  EBRequest
+//
+//  Created by Esteban on 01/10/12.
+//  Copyright (c) 2012 Esteban. All rights reserved.
+//
+
+#import "EBRequest.h"
+
+@interface EBJSONRequest : EBRequest
+
+@end
---
+++ EBRequest/EBRequest/EBJSONRequest.m
@@ -0,0 +1,13 @@
+//
+//  EBJSONRequest.m
+//  EBRequest
+//
+//  Created by Esteban on 01/10/12.
+//  Copyright (c) 2012 Esteban. All rights reserved.
+//
+
+#import "EBJSONRequest.h"
+
+@implementation EBJSONRequest
+
+@end
