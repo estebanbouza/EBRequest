62444abf5e5930a6f5dd805a3a87ad784c37ef21
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-10 23:03:56 +0200

    Improved documentation

--- EBRequest/EBRequest/EBPropertyMapper.h
+++ EBRequest/EBRequest/EBPropertyMapper.h
@@ -10,7 +10,22 @@
 
 
 /** The EBPropertyMapper class is used to associate the property names of a
- NSObject to the JSON property names. See property argumentMapper for more information.
+ NSObject to the JSON property names.
+ 
+ For example, for a class defined as
+ 
+ `Person(name, age)`
+ 
+ 
+ And a JSON defined as
+ 
+ `{ "jname" : "peter", "jage" : 35 }`
+ 
+ A mapper can be defined as:
+ 
+ 
+ `propertyMapper` `=` `@[@"name" : @"jname", @"age" : @"jage"];`
+ 
  */
 @interface EBPropertyMapper : NSObject
 
@@ -20,17 +35,7 @@
  */
 @property (nonatomic, retain) Class theClass;
 
-/** Dictionary containing an equivalence between JSON and Class property names.
- 
- 
- For example, for a class defined as
- Person(name, age)
- And a JSON defined as
- 
- { "jname" : "peter", "jage" : 35 }
- A mapper can be defined as:
- argumentMapper = [@"name" : @"jname", @"age" : @"jage"];
- */
+/** Dictionary containing an equivalence between JSON and Class property names. */
 @property (nonatomic, retain) NSDictionary *propertyMapper;
 
 
