8277ed0e151242dd5b401a929a3072814091dc7f
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-13 13:38:58 +0200

    Updated git version

--- EBRequest/EBRequest/EBGitVersion.h
+++ EBRequest/EBRequest/EBGitVersion.h
@@ -1,4 +1,4 @@
 /* Git repository information for current build: */
 
-#define kGitVersion       @"v0.2-2-g10daa37"
-#define kGitBranch        @"develop"
+#define kGitVersion       @"v0.3"
+#define kGitBranch        @"master"
