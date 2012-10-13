f1487a91d14dcaf497647b3e246d82a22aab0d80
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-13 05:26:15 +0200

    Updated documentation

--- EBRequest/EBRequest/EBJSONRequest.h
+++ EBRequest/EBRequest/EBJSONRequest.h
@@ -12,7 +12,7 @@
 /// EBJSONRequest returns JSON or Custom objects in the completion block. For more information see property classesToMap
 @interface EBJSONRequest : EBRequest
 
-/// Optional argument. If the parameter is non nil, the result of the mapping will be custom classes instead of NSDictionary and NSArrays. The mapping is done through EBJSONObjectMapper
+/// Optional argument. If the parameter is not nil, the result of the mapping will be custom classes instead of NSDictionary and NSArrays.
 @property (nonatomic, retain) EBJSONObjectMapper *JSONObjectMapper;
 
 @end
