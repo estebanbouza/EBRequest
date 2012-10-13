bd0cb2a72e6ca249074067fcbcb7a5abfe0c3d80
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-13 13:25:56 +0200

    Added documentation for EBDescribedObject. Headers set as public.

--- EBRequest/EBRequest/EBDescribedObject.h
+++ EBRequest/EBRequest/EBDescribedObject.h
@@ -8,6 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+/** EBDescribedObject subclasses have a default implementation for -description method.
+    
+ When calling -description in a EBDescribedObject subclass, all the properties will be printed in the console. See -description.
+ 
+ */
 @interface EBDescribedObject : NSObject
+
+/** The description value of all the properties.
+ 
+ The format for printing is:
+ 
+ - propertyName: {propertyValue}, propertyName: {propertyValue}, etc...
+ - All properties are sorted alphabetically.
+ 
+ */
+- (NSString *)description;
 
 @end
