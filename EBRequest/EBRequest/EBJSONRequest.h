fde3dbabcd99dfce7151d8fbac62dcb54463c854
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-13 21:16:11 +0200

    Updated documentation headers

--- EBRequest/EBRequest/EBJSONRequest.h
+++ EBRequest/EBRequest/EBJSONRequest.h
@@ -9,7 +9,7 @@
 #import "EBRequest.h"
 #import "EBJSONObjectMapper.h"
 
-/// EBJSONRequest returns JSON or Custom objects in the completion block. For more information see property classesToMap
+/// EBJSONRequest returns JSON or Custom objects in the completion block. For more information see property JSONObjectMapper
 @interface EBJSONRequest : EBRequest
 
 /// Optional argument. If the parameter is not nil, the result of the mapping will be custom classes instead of NSDictionary and NSArrays.
