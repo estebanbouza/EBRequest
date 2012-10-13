f11e6aba1506618d5f01ba7806c81671a5bda27e
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-13 21:01:30 +0200

    Added tests for EBImageRequest

--- EBRequest/EBRequest/EBGitVersion.h
+++ EBRequest/EBRequest/EBGitVersion.h
@@ -1,4 +1,4 @@
 /* Git repository information for current build: */
 
-#define kGitVersion       @"v0.3-8-g5d39fc3"
+#define kGitVersion       @"v0.3-10-g10fa16f"
 #define kGitBranch        @"feature-image-request"
--- EBRequest/EBRequestTests/EBImageRequestTests.m
+++ EBRequest/EBRequestTests/EBImageRequestTests.m
@@ -8,6 +8,7 @@
 
 #import "EBImageRequestTests.h"
 #import <UIKit/UIKit.h>
+#import "MockPerson.h"
 
 static const NSTimeInterval defaultTimeout = 10;
 
@@ -21,11 +22,15 @@ static const NSTimeInterval defaultTimeout = 10;
     EBImageRequest *imageRequest = [EBImageRequest requestWithURL:[NSURL URLWithString:@"https://www.google.com/images/srpr/logo3w.png"]];
     
     imageRequest.completionBlock = ^(id img) {
-        UIImage *image = (UIImage *)img;
-        DLog(@"Image downloaded: %@", image);
+        dispatch_semaphore_signal(semaphore);
+        completionExecuted = YES;
+
+        STAssertTrue([img isKindOfClass:[UIImage class]], nil);
     };
     
     imageRequest.errorBlock = ^(NSError *error) {
+        dispatch_semaphore_signal(semaphore);
+
         STFail(@"Error downloading image: %@", error);
     };
     
@@ -40,5 +45,69 @@ static const NSTimeInterval defaultTimeout = 10;
     dispatch_release(semaphore);
 
 }
+
+
+- (void)testDownloadInvalidURL {
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    __block BOOL errorExecuted = NO;
+    
+    EBImageRequest *imageRequest = [EBImageRequest requestWithURL:[NSURL URLWithString:@"http://localhost:666/void.jpg"]];
+    
+    imageRequest.completionBlock = ^(id img) {
+        dispatch_semaphore_signal(semaphore);
+        
+        STFail(@"This request should fail");
+    };
+    
+    imageRequest.errorBlock = ^(NSError *error) {
+        dispatch_semaphore_signal(semaphore);
+        
+        errorExecuted = YES;
+        STAssertNotNil(error, @"An error should be raised");
+    };
+    
+    [imageRequest start];
+    
+    while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW)) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:defaultTimeout]];
+    }
+    
+    STAssertTrue(errorExecuted, nil);
+    
+    dispatch_release(semaphore);
+}
+
+
+- (void)testDownloadWrongImage {
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    __block BOOL errorExecuted = NO;
+    
+    EBImageRequest *imageRequest = [EBImageRequest requestWithURL:[NSURL URLWithString:@"http://dl.dropbox.com/u/425260/EBRequestInvalidImage.png"]];
+    
+    imageRequest.completionBlock = ^(id img) {
+        dispatch_semaphore_signal(semaphore);
+        
+        STFail(@"This request should fail");
+    };
+    
+    imageRequest.errorBlock = ^(NSError *error) {
+        dispatch_semaphore_signal(semaphore);
+        
+        errorExecuted = YES;
+        STAssertNotNil(error, @"An error should be raised");
+        STAssertTrue(error.code == -1, @"Invalid error code: %@", error);
+    };
+    
+    [imageRequest start];
+    
+    while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW)) {
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:defaultTimeout]];
+    }
+    
+    STAssertTrue(errorExecuted, nil);
+    
+    dispatch_release(semaphore);
+}
+
 
 @end
