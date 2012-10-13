565a218ee8d3b2136b6ad7b274c7181a65276a2d
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-13 13:30:41 +0200

    Extracted runtime utils to common class

--- EBRequest/EBRequest/EBDescribedObject.m
+++ EBRequest/EBRequest/EBDescribedObject.m
@@ -7,17 +7,15 @@
 //
 
 #import "EBDescribedObject.h"
-#import <objc/runtime.h>
-
+#import "EBRuntimeUtils.h"
 
 @implementation EBDescribedObject
 
 - (NSString *)description {
     NSMutableString *result = [NSMutableString string];
     
-    // Sort properties by name:
-    
-    NSMutableSet *properties = [self propertyNamesForClass:[self class]];
+    // Sort properties by name
+    NSMutableSet *properties = [EBRuntimeUtils propertyNamesForClass:[self class]];
     NSArray *sortedProperties = [[properties allObjects] sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
         return [obj1 compare:obj2];
     }];
@@ -32,23 +30,6 @@
     return result;
 }
 
-- (NSMutableSet *)propertyNamesForClass:(Class)class {
-    
-    unsigned int count;
-    
-    objc_property_t *properties = class_copyPropertyList(class, &count);
-    
-    NSMutableSet *propertyNames = [NSMutableSet setWithCapacity:count];
-    
-    for (int i = 0; i < count; i++) {
-        objc_property_t property = properties[i];
-        
-        NSString *propertyName = [NSString stringWithCString:property_getName(property) encoding:NSUTF8StringEncoding];
-        
-        [propertyNames addObject:propertyName];
-    }
-    
-    return propertyNames;
-}
+
 
 @end
