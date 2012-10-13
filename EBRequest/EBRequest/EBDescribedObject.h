3a8501edc9019c57d52b973d22874f805e8bad15
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-13 05:37:32 +0200

    Added class for self described object

---
+++ EBRequest/EBRequest/EBDescribedObject.h
@@ -0,0 +1,13 @@
+//
+//  EBDescribedObject.h
+//  EBRequest
+//
+//  Created by Esteban on 13/10/12.
+//  Copyright (c) 2012 Esteban. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EBDescribedObject : NSObject
+
+@end
---
+++ EBRequest/EBRequest/EBDescribedObject.m
@@ -0,0 +1,17 @@
+//
+//  EBDescribedObject.m
+//  EBRequest
+//
+//  Created by Esteban on 13/10/12.
+//  Copyright (c) 2012 Esteban. All rights reserved.
+//
+
+#import "EBDescribedObject.h"
+
+@implementation EBDescribedObject
+
+- (NSString *)description {
+    return @"nothing";
+}
+
+@end
