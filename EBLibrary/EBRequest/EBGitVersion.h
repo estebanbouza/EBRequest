9efd7be851382bdd9dfc39f1ec6b204d57d10833
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-20 18:17:09 +0200

    Removed unused build phase

--- EBLibrary/EBRequest/EBGitVersion.h
+++ EBLibrary/EBRequest/EBGitVersion.h
@@ -1,4 +1,4 @@
 /* Git repository information for current build: */
 
-#define kGitVersion       @"v0.3-55-ge22ea4d"
+#define kGitVersion       @"v0.3-56-g4565b53"
 #define kGitBranch        @"feature-library-as-framework"
