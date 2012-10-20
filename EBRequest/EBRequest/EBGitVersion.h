e6067b53be2f497cced493473a7c6635ec90cf47
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-20 17:22:15 +0200

    Added script to generate a .framework structure

--- EBRequest/EBRequest/EBGitVersion.h
+++ EBRequest/EBRequest/EBGitVersion.h
@@ -1,4 +1,4 @@
 /* Git repository information for current build: */
 
-#define kGitVersion       @"v0.3-50-gb7210d8"
+#define kGitVersion       @"v0.3-51-g4d93e35"
 #define kGitBranch        @"feature-library-as-framework"
