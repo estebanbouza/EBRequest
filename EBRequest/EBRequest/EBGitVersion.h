4d93e35f4764e54b4f3f048164e9b8c086949eef
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-20 17:13:02 +0200

    Changed build settings

--- EBRequest/EBRequest/EBGitVersion.h
+++ EBRequest/EBRequest/EBGitVersion.h
@@ -1,4 +1,4 @@
 /* Git repository information for current build: */
 
-#define kGitVersion       @"v0.3-49-gde9dca1"
+#define kGitVersion       @"v0.3-50-gb7210d8"
 #define kGitBranch        @"feature-library-as-framework"
