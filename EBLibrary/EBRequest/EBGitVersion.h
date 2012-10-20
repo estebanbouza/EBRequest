e22ea4d142bed4c0337229ebfba9fab4d53cf554
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-20 17:54:29 +0200

    Added default script from https://github.com/jverkoey/iOS-Framework#walkthrough. Not compiling

--- EBLibrary/EBRequest/EBGitVersion.h
+++ EBLibrary/EBRequest/EBGitVersion.h
@@ -1,4 +1,4 @@
 /* Git repository information for current build: */
 
-#define kGitVersion       @"v0.3-53-g364df10"
+#define kGitVersion       @"v0.3-54-g1f78ed5"
 #define kGitBranch        @"feature-library-as-framework"
