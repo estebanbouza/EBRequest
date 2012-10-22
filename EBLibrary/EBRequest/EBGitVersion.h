7fd850a42f64c0087c71171ddc8544ed58dd0f2c
Esteban Bouza <esteban.bouza@gmail.com>
2012-10-22 22:55:40 +0200

    Updated documentation

--- EBLibrary/EBRequest/EBGitVersion.h
+++ EBLibrary/EBRequest/EBGitVersion.h
@@ -1,4 +1,4 @@
 /* Git repository information for current build: */
 
-#define kGitVersion       @"v0.3-89-g3528d39"
+#define kGitVersion       @"v0.3-94-g7f947a7"
 #define kGitBranch        @"develop"
